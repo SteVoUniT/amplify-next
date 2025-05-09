bc7050f7e9f47ba45bb6b97bed6caceee342faa6
Steven Alvarez <stevounit@gmail.com>
2025-05-09 17:13:33 -0400

    First deployment

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,12 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+//amplify/backend.ts
+
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource.js";
+import { data } from "./data/resource.js";
+import { storage } from "./storage/resource.js";
 
 defineBackend({
   auth,
   data,
+  storage,
 });
---
+++ amplify/storage/resource.ts
@@ -0,0 +1,12 @@
+// amplify/storage/resource.ts
+
+import { defineStorage } from "@aws-amplify/backend";
+
+export const storage = defineStorage({
+  name: "todosStorage",
+  access: (allow) => ({
+    "media/*": [
+      allow.guest.to(["read", "write", "delete"]), // additional actions such as "write" and "delete" can be specified depending on your use case
+    ],
+  }),
+});
